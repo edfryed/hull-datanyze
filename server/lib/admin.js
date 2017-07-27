dca92a4b6580b4c4db42facb7633ef750f8a0136
Sven Maschek <sven.maschek@outlook.de>
2017-07-26 20:19:38 -0400

    First draft for DEV-14

--- server/lib/admin.js
+++ server/lib/admin.js
@@ -3,19 +3,25 @@ import rest from "restler";
 import { Request, Response } from "express";
 export default function admin(req: Request, res: Response) {
   const { username, token } = req.hull.ship.private_settings;
-  rest
-    .get("http://api.datanyze.com/limits/", { query: { email: username, token } })
-    .on("success", function onLimitSucces(limits = {}) {
-      req.hull.client.logger.debug("datanyze.rate.debug", limits);
-      res.render("admin.html", {
-        limits,
-        progress: {
-          hourly: Math.ceil((limits.api_hourly / limits.api_hourly_limit) * 100),
-          daily: Math.ceil((limits.api_daily / limits.api_daily_limit) * 100),
-          monthly: Math.ceil((limits.api_monthly / limits.api_monthly_limit) * 100)
-        },
-        connected: (!!token && !!username && !!limits.api_monthly),
-        name: "Datanyze"
+  const connectorName = "Datanyze";
+  console.log(req.hull.ship.private_settings);
+  if (!username || !token) {
+    res.render("noauthconfig.html", { name: connectorName });
+  } else {
+    rest
+      .get("http://api.datanyze.com/limits/", { query: { email: username, token } })
+      .on("success", function onLimitSucces(limits = {}) {
+        req.hull.client.logger.debug("datanyze.rate.debug", limits);
+        res.render("admin.html", {
+          limits,
+          progress: {
+            hourly: Math.ceil((limits.api_hourly / limits.api_hourly_limit) * 100),
+            daily: Math.ceil((limits.api_daily / limits.api_daily_limit) * 100),
+            monthly: Math.ceil((limits.api_monthly / limits.api_monthly_limit) * 100)
+          },
+          connected: (!!token && !!username && !!limits.api_monthly),
+          name: connectorName
+        });
       });
-    });
+  }
 }
